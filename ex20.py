016886240f246ed9b5542a23b4d2cde523c893be
Alok Rout <alok.rout@outlook.com>
2017-09-15 18:36:10 +0530

    More Functions

--- ex20.py
+++ ex20.py
@@ -6,11 +6,12 @@ def print_all(f):
     print f.read()
 
 def rewind(f):
-    #f.seek(2) #Seeks position 2 from the default (zero) position
-    f.seek(-1, 2) #the second parameter indicates that the interpreter will seek from the end. Look up 'whence'.
+    f.seek(0) #Seeks position 2 from the default (zero) position
+    #f.seek(-10, 2) #the second parameter indicates that the interpreter will seek from the end. Look up 'whence'.
 
 def print_a_line(line_count, f):
-    print line_count, f.readline()
+    print line_count , f.readline()
+    #print line_count , f.readline(), # The comma at end prevents print statement from adding an implicit \n to the output.
 
 current_file = open(input_file)
 
@@ -24,11 +25,9 @@ print "Let's print three lines:"
 
 current_line = 1
 print_a_line(current_line, current_file)
-'''
-current_line += 1
-print_a_line(current_line, current_file)
 
 current_line += 1
 print_a_line(current_line, current_file)
 
-'''
+current_line += 1
+print_a_line(current_line, current_file)
